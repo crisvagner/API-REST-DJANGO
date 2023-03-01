cc8a0eb4290db9f69c53b118bbad3c4424b7a54f
eokrizz <crisvagnersd@gmail.com>
2023-02-28 21:14:59 -0300

    configura rotas da aplicação no urls.py principal

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -1,4 +1,5 @@
-"""ToDoList URL Configuration
+from Users.api.viewsets import UsersViewSet, CreateUserView
+from Notes.api.viewsets import NotesViewSet
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -16,8 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Importa o pacote routers do Django REST Framework
+from rest_framework import routers
+
+# Cria um objeto router do tipo DefaultRouter do Django REST Framework
+router = routers.DefaultRouter()
+
+# Registra as rotas para os ViewSets UsersViewSet e NotesViewSet no router
+router.register(r'users', UsersViewSet, basename='Users')
+router.register(r'notes', NotesViewSet, basename='Notes')
+
+# Define as rotas da aplicação
 urlpatterns = [
+    # Rota para a página de administração do Django
     path('admin/', admin.site.urls),
-    path('users/', include('Users.urls')),
-    path('notes/', include('Notes.urls'))
+    # Inclui as rotas definidas no router
+    path('', include(router.urls)),
+    # Rota para a página de login da aplicação
+    path('login/', include('Users.urls')),
+    # Rota para a página de registro de usuários da aplicação
+    path('register/', CreateUserView.as_view()),
 ]
