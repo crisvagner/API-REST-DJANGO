f5d47773e327eaba678ca1c1594a07126e4e19ce
eokrizz <crisvagnersd@gmail.com>
2023-02-28 21:26:29 -0300

    cria model para o app Notes

--- Notes/models.py
+++ Notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Define uma classe Notes que estende a classe Model do Django
+class Notes(models.Model):
+    # Define o campo title como um campo CharField de tamanho máximo 50
+    title = models.CharField(max_length=50)
+    # Define o campo content como um campo TextField
+    content = models.TextField()
+    # Define o campo author como um campo IntegerField
+    author_id = models.IntegerField()
+
+    # Método para retornar o título da nota
+    def __str__(self):
+        return self.title
